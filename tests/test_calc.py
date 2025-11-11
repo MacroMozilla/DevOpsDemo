c68092b39968983700d5e255e149ba26a3ce4812
harry <hz3866@nyu.edu>
2025-11-11 00:58:25 -0500

    Add subtraction function and update tests and workflow
    Introduced a `sub` function in `trivial_tools.py` and added its corresponding unit test in `test_calc.py`. Additionally, improved the Slack notification in the GitHub Actions workflow to include the commit message for better context.

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -6,6 +6,10 @@ class TestCalc(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
 
+    def test_sub(self):
+        self.assertEqual(sub(1, 2), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
+   
