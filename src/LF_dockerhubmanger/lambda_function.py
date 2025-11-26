fcb8c3493b9687fd84f381bc8540b2f4b1b06df2
harry <hz3866@nyu.edu>
2025-11-25 21:46:40 -0500

    Rename module and add AWS Lambda handler for DockerHub
    Renamed `docker_tools` module to align with `LF_dockerhubmanger` structure. Introduced a new `lambda_function.py` script to serve as an AWS Lambda handler, enabling repository and tag management functions.

---
+++ src/LF_dockerhubmanger/lambda_function.py
@@ -0,0 +1,38 @@
+import json
+
+from docker_tools import *
+
+manager = DockerHubManager()
+
+
+def get_repos():
+    repos = manager.get_repos()
+    return repos
+
+
+def get_tags_by_repo():
+    tags = manager.get_tags_by_repo(repo)
+    return tags
+
+
+fname2fctn = {'get_tags_by_repo': get_tags_by_repo,
+              'get_repos': get_repos}
+
+
+def lambda_handler(event, context):
+    fname = event['fctn']
+    args = tuple(event.get('args', []))
+    kwargs = event.get('kwargs', {})
+
+    if fname in fname2fctn:
+        result = fname2fctn[fname](*args, **kwargs)
+
+        return {
+            'statusCode': 200,
+            'result': result
+        }
+    else:
+        return {
+            'statusCode': 404,
+            'msg': 'Function not found!'
+        }
